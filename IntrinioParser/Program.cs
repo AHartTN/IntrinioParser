2f0c146ee369046493efe79a3ec56b993834ab66
Anthony Hart <anthony@anthonyhart.info>
2018-02-21 14:41:35 -0600

    Add project files.

---
+++ IntrinioParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntrinioParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
